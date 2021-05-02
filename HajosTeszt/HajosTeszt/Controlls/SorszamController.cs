4ff94fcb69f95f2d19be93084b4b8bdb381c4d14
corvinus-tgpztt <mate.szladek@stud.uni-corvinus.hu>
2021-05-02 18:26:14 +0200

    hatékonyság növelése

---
+++ HajosTeszt/HajosTeszt/Controlls/SorszamController.cs
@@ -0,0 +1,25 @@
+﻿using HajosTeszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controlls
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class SorszamController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() //Tetszőleges metódusnév
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
+    }
+}
--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,24 +1,90 @@
-﻿var kérdések;
-var sorszam = 2;
+﻿
+var kérdés;
+var hotList = [];           //Az éppen gyakoroltatott kérdések listája 
+var questionsInHotList = 3; //Ez majd 7 lesz, teszteléshez jobb a 3. 
+var displayedQuestion;      //A hotList-ből éppen ez a kérdés van kint
+var numberOfQuestions;      //Kérdések száma a teljes adatbázisban
+var nextQuestion = 1;       //A következő kérdés száma a teljes listában
+var timeoutHandler;
+var repeat = 3;
 var kattinthatoe = true;
+var timeoutHandler;
+var maxkerdes;
+fetch('/questions/count').then((response) => response.json()).then(q => maxment(q));
+function maxment(szam) {
+    maxkerdes = szam;
+}
+window.onload = init();
+function init() {
 
+   
+        
 
-window.onload= function letöltés(){
-   /* fetch('/questions.json')
-        .then(response => response.json())
-        .then(data => letöltésBefejeződött(data)
-        );
+    let mystorage = window.localStorage.getItem("savedlist");
+    if (mystorage) {
+        console.log("Volt már listánk")
+        for (var i = 0; i < questionsInHotList; i++) {
+            let q = {
+                question: {},
+                goodAnswers: 0
+            }
+            hotList[i] = q;
+        }
+        hotList = JSON.parse(mystorage);
+        displayedQuestion = parseInt(window.localStorage.getItem("megjelenitett"));
+        nextQuestion = window.localStorage.getItem("holtartunk");
+        kérdésMegjelenítés();
+    }
+    else {
+        //Első kérdések letöltése
+        for (var i = 0; i < questionsInHotList; i++) {
+            let q = {
+                question: {},
+                goodAnswers: 0
+            }
+            hotList[i] = q;
+        }
+        for (var i = 0; i < questionsInHotList; i++) {
+            kérdésBetöltés(nextQuestion, i);
+            nextQuestion++;
+        }
+    }
 
-    function letöltésBefejeződött(d) {
-        console.log("Sikeres letöltés")
-        console.log(d)
-        kérdések = d;
-        kérdésMegjelenítés(kérdések[0])
-    } */
-    kérdésBetöltés(sorszam)
+    
+
     
 }
-function kérdésBetöltés(id) {
+
+   
+function kérdésBetöltés(questionNumber, destination) {
+    fetch(`/questions/${questionNumber}`)
+        .then(
+            result => {
+                if (!result.ok) {
+                    console.error(`Hibás letöltés: ${response.status}`)
+                }
+                else {
+                    return result.json()
+                }
+            }
+        )
+        .then(
+            q => {
+                hotList[destination].question = q;
+                hotList[destination].goodAnswers = 0;
+                console.log(`A ${questionNumber}. kérdés letöltve a hot list ${destination}. helyére`)
+                if (displayedQuestion == undefined && destination == 0) {
+                    displayedQuestion = 0;
+                    kérdésMegjelenítés();
+                }
+            }
+        );
+}
+
+
+
+
+/*function kérdésBetöltés2(id) {
             fetch(`/questions/${id}`)
                 .then(response => {
                     if (!response.ok) {
@@ -30,26 +96,31 @@ function kérdésBetöltés(id) {
                 })
                 .then(data => letöltésBefejeződött(data));                
 }    
-function letöltésBefejeződött(d) {
+*/
+/*function letöltésBefejeződött(d) {
     console.log("Sikeres letöltés")
     console.log(d)
     kérdések = d;
     kérdésMegjelenítés(kérdések)
-}
+}*/
 
-function kérdésMegjelenítés(kérdés) {
+function kérdésMegjelenítés() {
+    var kérdés = hotList[displayedQuestion].question;
     console.log(kérdés);
-    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
-    document.getElementById("válasz1").innerText = kérdés.answer1
-    document.getElementById("válasz2").innerText = kérdés.answer2
-    document.getElementById("válasz3").innerText = kérdés.answer3   
+    window.localStorage.setItem("megjelenitett", displayedQuestion);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText;
+    document.getElementById("válasz1").innerText = kérdés.answer1;
+    document.getElementById("válasz2").innerText = kérdés.answer2;
+    document.getElementById("válasz3").innerText = kérdés.answer3;
     if (kérdés.image == "") {
         document.getElementById("kép1").src = "";
     }
     else {
         document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;    
     }
-         
+    
+    document.getElementById("kerdesszam").innerText = (hotList[displayedQuestion].question.questionId) + ". kérdés" ;
+    document.getElementById("kor").innerText = hotList[displayedQuestion].goodAnswers + "/" + repeat;    
 }
 
 function visszaallit() {
@@ -67,14 +138,40 @@ function ertekeles(szam) {
         document.getElementById("válasz2").classList = "kerdes megvalaszolt";
         document.getElementById("válasz3").classList = "kerdes megvalaszolt";
 
-        if (szam == kérdések.correctAnswer) {
+        if (szam == hotList[displayedQuestion].question.correctAnswer) {
             document.getElementById("válasz" + szam).classList = "kerdes jó";
+            hotList[displayedQuestion].goodAnswers++;
+            window.localStorage.setItem("holtartunk", nextQuestion);
+            window.localStorage.setItem("savedlist", JSON.stringify(hotList));            
+           console.log("localstorage mentve");
         }
         else {
             document.getElementById("válasz" + szam).classList = "kerdes rossz";
-            document.getElementById("válasz" + kérdések.correctAnswer).classList = "kerdes jó";
+            hotList[displayedQuestion].goodAnswers=0;
+            document.getElementById("válasz" + hotList[displayedQuestion].question.correctAnswer).classList = "kerdes jó";
+            window.localStorage.setItem("holtartunk", nextQuestion);
+          console.log(window.localStorage.getItem("savedlist"));
         }
         kattinthatoe = false;
+
+
+        if (hotList[displayedQuestion].goodAnswers >= repeat)
+        {
+            if (nextQuestion>= maxkerdes-1) {
+                alert("Nincs már több kérdés!");
+                timeoutHandler = setTimeout(reset, 3000);
+            }
+            else {
+                kérdésBetöltés(nextQuestion, displayedQuestion);
+                hotList[displayedQuestion].goodAnswers = 0;
+                nextQuestion++;
+                window.localStorage.setItem("holtartunk", nextQuestion);
+                window.localStorage.setItem("savedlist", JSON.stringify(hotList));
+            }
+            
+        }
+        timeoutHandler = setTimeout(elore, 3000);
+
     }
 
     
@@ -82,21 +179,28 @@ function ertekeles(szam) {
 
 
 function elore() {
-
-    if (sorszam >= 800) {
-        sorszam = 0;
-    }
-
-    sorszam++;
-    kérdésBetöltés(sorszam);
+    clearTimeout(timeoutHandler)
     visszaallit();
+    displayedQuestion++;
+    if (displayedQuestion == questionsInHotList) displayedQuestion = 0;
+    kérdésMegjelenítés()
+    
 }
 function vissza() {
-    if (sorszam <= 1) {
-        sorszam = 801;
-    }
-
-    sorszam--; 
-    kérdésBetöltés(sorszam);
+    clearTimeout(timeoutHandler)
     visszaallit();
+    displayedQuestion--;   
+    if (displayedQuestion == -1) displayedQuestion = questionsInHotList-1;
+    kérdésMegjelenítés()   
+}
+function reset() {
+    localStorage.clear();
+    for (var i = 0; i < questionsInHotList; i++) {
+        hotList[i] = null
+        
+    }
+    nextQuestion = 1;
+    init();
+
+
 }
